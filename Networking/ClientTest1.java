70e1132e076fcf2f488a6869d37caa20b0bac7cd
bradyap <bpettengill3@gmail.com>
2020-12-18 08:58:51 -0500

    ehhhhhhh

---
+++ Networking/ClientTest1.java
@@ -0,0 +1,43 @@
+import java.net.*;
+import java.io.*;
+import java.util.Scanner;
+
+public class ClientTest1 {
+    public static void Client(String ip, int port) {
+        try {
+            //connect to server
+            Socket client = new Socket(ip, port);
+            System.out.println("Connected to " + ip + " on port " + port + ".");
+            
+            //sending input to server
+            DataOutputStream toServer = new DataOutputStream(client.getOutputStream());
+            toServer.writeUTF(UserIn);
+
+            //get and print response from server
+            DataInputStream fromServer = new DataInputStream(client.getInputStream());
+            System.out.println(fromServer.readUTF());
+
+            //close connection
+            client.close();
+        }
+        catch(IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String [] args) {
+        //input for ip and port
+        Scanner scanner = new Scanner(System.in);
+        scanner.useDelimiter(System.lineSeparator());
+        System.out.println("Enter target IP:");
+        String ip = scanner.next();
+        System.out.println("Enter target port:");
+        int port = scanner.nextInt();
+        System.out.println("Enter input to server:");
+        String userIn = scanner.next();
+        scanner.close();
+
+        //call client
+        Client(ip, port, userIn);
+    }
+}
---
+++ Networking/ServerTest1.java
@@ -0,0 +1,48 @@
+import java.net.*;
+import java.io.*;
+import java.util.Scanner;
+
+public class ServerTest1 {
+    public static void Server(int port) {
+        try {
+            //starts server, checks for and accepts client
+            ServerSocket serverSocket = new ServerSocket(port);
+            System.out.println("Server started - waiting for client...");
+            Socket server = serverSocket.accept();
+            System.out.println("Client connected.");
+
+            //get and print input from client
+            DataInputStream fromClient = new DataInputStream(server.getInputStream());
+            BufferedReader inputReader = new BufferedReader(new InputStreamReader(fromClient));
+            String inputLine;
+            while ((inputLine = inputReader.readLine()) != null) {
+                System.out.println(inputLine);
+                break;
+            }
+            DataOutputStream toClient = new DataOutputStream(server.getOutputStream());
+            fromClient.transferTo(toClient);
+            
+            
+            //send response to client
+            
+            //toClient.writeUTF("Server recieved:\n" + clientIn + "\nConnection to " + server.getLocalAddress() + " on port " + server.getLocalPort() + " complete.");
+
+            //close connection
+            //server.close();
+            //serverSocket.close();
+        }
+        catch(IOException e) {
+            e.printStackTrace();
+        }
+    }
+    public static void main(String [] args) {
+        //input for port
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter port:");
+        int port = scanner.nextInt();
+        scanner.close();
+
+        //call client
+        Server(port);
+    }
+}
