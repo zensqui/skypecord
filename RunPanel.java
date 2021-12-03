2a96f646ecf92effb7bdd2e22f1a849bf4e0090a
mwag2022 <56901031+mwag2022@users.noreply.github.com>
2021-12-03 11:00:26 -0500

    Drawing

---
+++ RunPanel.java
@@ -0,0 +1,32 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+import java.awt.BorderLayout;
+
+public class RunPanel
+{
+   public static void main(String[] args)
+   {
+      JFrame frame = new JFrame("Panel");
+      
+      frame.setDefaultCloseOperation(3);
+      frame.setLocation(0, 0);
+      
+      Panel panel = new Panel();
+      Panel1 panel1 = new Panel1();
+      Panel2 panel2 = new Panel2();
+      
+      frame.add(panel);
+      frame.add(panel1);
+      frame.add(panel2);
+      
+      frame.add(panel, BorderLayout.WEST);
+      frame.add(panel1, BorderLayout.CENTER);
+      frame.add(panel2, BorderLayout.EAST);
+      
+      frame.pack();
+      frame.setVisible( true );
+    
+   }
+}
