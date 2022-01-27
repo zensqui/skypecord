8a6305822cc35bc359ecfa54aa671b095844641f
bradyap <bpettengill3@gmail.com>
2022-01-27 11:46:19 -0500

    client no work

--- client/app/Client.java
+++ client/app/Client.java
@@ -22,5 +22,10 @@ public class Client {
         json.put("pass", pass);
         out.append(json.toJSONString() + "\n");
         out.flush();
-    }  
+    }
+
+    public static void main(String[] args) throws IOException, ParseException {
+        Client client = new Client();
+        client.login("test", "test");
+    }
 }
