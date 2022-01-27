7385c788bd70ba35dd7f8b2bff2ab7072554dd50
bradyap <bpettengill3@gmail.com>
2022-01-27 14:31:47 -0500

    client

--- client/app/Client.java
+++ client/app/Client.java
@@ -9,6 +9,8 @@ public class Client {
     private BufferedReader in;
     private OutputStreamWriter out;
 
+    private String user;
+
     public Client() throws IOException {
         client = new Socket("sc.zenithproject.xyz", 5050);
         in = new BufferedReader(new InputStreamReader(client.getInputStream()));
@@ -22,12 +24,26 @@ public class Client {
         json.put("pass", pass);
         out.append(json.toJSONString() + "\n");
         out.flush();
+
+        this.user = user;
     }
 
-    public static void main(String[] args) throws IOException, ParseException {
-        System.out.println("Running...");
-        Client client = new Client();
-        client.login("test", "test");
-        System.out.println("Done.");
+    public void register(String user, String pass) throws IOException, ParseException {
+        JSONObject json = new JSONObject();
+        json.put("type", "register");
+        json.put("user", user);
+        json.put("pass", pass);
+        out.append(json.toJSONString() + "\n");
+        out.flush();
+    }
+
+    public void message(String type, String target, String data) throws IOException, ParseException {
+        JSONObject json = new JSONObject();
+        json.put("type", type);
+        json.put("user", user);
+        json.put("target", target);
+        json.put("data", data);
+        out.append(json.toJSONString() + "\n");
+        out.flush();
     }
 }
