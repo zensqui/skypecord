2c0cb9f2574b4142cfe315b01ef0b0f1ae0e0ff0
bradyap <bpettengill3@gmail.com>
2022-02-02 17:47:15 +0000

    clienttest

--- client/app/ClientTest.java
+++ client/app/ClientTest.java
@@ -1,6 +1,19 @@
 public class ClientTest {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
         Client client = new Client();
         
+        client.register("test0", "test0");
+        client.register("test1", "test1");
+        client.register("test2", "test2");
+        client.register("test3", "test3");
+        client.register("test4", "test4");
+        client.register("test5", "test5");
+
+        client.login("test0", "test0");
+        client.login("test1", "test1");
+        client.login("test2", "test2");
+        client.login("test3", "test3");
+        client.login("test4", "test4");
+        client.login("test5", "test5");
     }
 }
