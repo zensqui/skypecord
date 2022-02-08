97edfb5f922cdb21a1b45292f6b68c9c39644003
mwag2022 <56901031+mwag2022@users.noreply.github.com>
2022-02-08 10:47:56 -0500

    Add files via upload
    idk

--- client/app/messageInput.java
+++ client/app/messageInput.java
@@ -12,7 +12,8 @@ public class messageInput extends JFrame implements ActionListener{
     ArrayList<String> data;
     DefaultListModel<String> model;
     int maxSize;
-
+    String user;
+    String targetUser;
     public messageInput(){
 
         SpringLayout layout = new SpringLayout();
@@ -22,13 +23,15 @@ public class messageInput extends JFrame implements ActionListener{
 
         model = new DefaultListModel<>();
 
-         
+         user = "Morgan";
+
+         targetUser = "Brady";
 
         send = new JButton();
         send.setText("Send");
         
-        
-        model.addElement("New Conversation");
+        //the first element should have the user you are talking to
+        model.addElement("Chat with " + targetUser);
         model.addElement("   ");
         list = new JList<>(model);
         
@@ -36,6 +39,8 @@ public class messageInput extends JFrame implements ActionListener{
         scrollPane.setViewportView(list);
         list.setLayoutOrientation(JList.VERTICAL);
 
+
+        //scroll pane scrolls to the bottem when model is updated
          maxSize = scrollPane.getVerticalScrollBar().getMaximum();
          scrollPane.getVerticalScrollBar().addAdjustmentListener(e -> {
             if ((maxSize - e.getAdjustable().getMaximum()) == 0)
@@ -44,24 +49,24 @@ public class messageInput extends JFrame implements ActionListener{
             maxSize = scrollPane.getVerticalScrollBar().getMaximum();
          });
       
-        layout.putConstraint(SpringLayout.HORIZONTAL_CENTER, scrollPane, 90, SpringLayout.HORIZONTAL_CENTER, msgInpt);
+        layout.putConstraint(SpringLayout.HORIZONTAL_CENTER, scrollPane, 0, SpringLayout.HORIZONTAL_CENTER, msgInpt);
         layout.putConstraint(SpringLayout.NORTH, scrollPane, 0, SpringLayout.NORTH, msgInpt);
-        scrollPane.setPreferredSize(new Dimension(300, 400));
+        scrollPane.setPreferredSize(new Dimension(400, 400));
         msgInpt.add(scrollPane);
         
         message = new JTextField();
-        layout.putConstraint(SpringLayout.HORIZONTAL_CENTER, message, 65, SpringLayout.HORIZONTAL_CENTER, msgInpt);
+        layout.putConstraint(SpringLayout.HORIZONTAL_CENTER, message, -50, SpringLayout.HORIZONTAL_CENTER, msgInpt);
         layout.putConstraint(SpringLayout.NORTH, message, 400, SpringLayout.NORTH, msgInpt);
-        message.setPreferredSize(new Dimension(250, 50));
+        message.setPreferredSize(new Dimension(300, 50));
         message.addKeyListener(new keyListener());
         msgInpt.add(message);
          
-
+         
 
         send = new JButton("Send");
-        layout.putConstraint(SpringLayout.HORIZONTAL_CENTER, send, 215, SpringLayout.HORIZONTAL_CENTER, msgInpt);
+        layout.putConstraint(SpringLayout.HORIZONTAL_CENTER, send, 150, SpringLayout.HORIZONTAL_CENTER, msgInpt);
         layout.putConstraint(SpringLayout.NORTH, send, 400, SpringLayout.NORTH, msgInpt);
-        send.setPreferredSize(new Dimension(50, 50));
+        send.setPreferredSize(new Dimension(100, 50));
         send.addActionListener(this);
         msgInpt.add(send);
 
@@ -71,6 +76,7 @@ public class messageInput extends JFrame implements ActionListener{
         setVisible(true);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
+
     public static void main(String[] args) {
         new messageInput();
     }
@@ -81,7 +87,7 @@ public class messageInput extends JFrame implements ActionListener{
 
       if((JButton)e.getSource() == send){
          if(!(message.getText() == null)){
-            model.addElement(message.getText());
+            model.addElement(user + ": " + message.getText());
          }
 
       }
@@ -99,8 +105,8 @@ private class keyListener implements KeyListener{
    @Override
    public void keyPressed(KeyEvent e) {
       if (e.getKeyCode() == KeyEvent.VK_ENTER) {
-         if(!(message.getText() == null)){
-            model.addElement(message.getText());
+         if(!(message.getText().equals(""))){
+            model.addElement(user + ": " + message.getText());
          }
          System.out.println(message.getText());
          message.setText("");
